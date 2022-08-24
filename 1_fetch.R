05d25888c47f71b352bc23cf21a524177788748a
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-08-24 14:45:49 -0500

    Change to updated filename on SB

--- 1_fetch.R
+++ 1_fetch.R
@@ -11,21 +11,12 @@ p1_targets <- list(
   
   ##### Fetch data from child item 1951-2020 #####
   
-  ###### Download drought properties files ######
-  tar_target(p1_1951_2020_drought_prop_site_csv, {
-    # Depend on this dummy variable to initiate re-download of files
-    p0_sb_fetch_date
-    download_sb_files(sb_id = p0_sbitem_child_1951_2020,
-                      sb_files_to_download = 'Drought_Properties_site_updated.csv',
-                      dest_dir = "1_fetch/out/CONUS_1951_2020",
-                      sb_secret_exists = p0_sb_credentials_exist)
-  },
-  format = 'file'),
+  ###### Download drought properties file ######
   tar_target(p1_1951_2020_drought_prop_jd_7d_csv, {
     # Depend on this dummy variable to initiate re-download of files
     p0_sb_fetch_date
     download_sb_files(sb_id = p0_sbitem_child_1951_2020,
-                      sb_files_to_download = 'Drought_Properties_jd_07d_wndw_updated.csv',
+                      sb_files_to_download = 'Drought_Properties_jd_07d_wndw.csv',
                       dest_dir = "1_fetch/out/CONUS_1951_2020",
                       sb_secret_exists = p0_sb_credentials_exist)
   },
