0b27e4adf8a17592ada3354dcf830482098cbb2b
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-08-25 17:16:58 -0500

    Use compressed version in site

--- 3_visualize.R
+++ 3_visualize.R
@@ -20,7 +20,7 @@ p3_targets <- list(
     event_swarm_plot_compressed(swarm_data = p2_prop_western_2_swarm_compressed)
   ),
   tar_target(p3_prop_western_2_swarm_compressed_png,
-             ggsave('3_visualize/out/swarm_jd7d_2_western_compressed.png', 
+             ggsave('src/assets/images/duration-chart/swarm_jd7d_2_western_compressed.png', 
                     p3_prop_western_2_swarm_plot_compressed,
                     width = 100, height = 10, dpi = 300, limitsize = FALSE),
              format = "file" )
