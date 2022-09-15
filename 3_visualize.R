5073a79e481b9c33234b58786987fe11d32a5a1a
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-09-15 14:12:31 -0500

    Add vertical plot target

--- 3_visualize.R
+++ 3_visualize.R
@@ -23,5 +23,16 @@ p3_targets <- list(
              ggsave('src/assets/images/duration-chart/swarm_jd7d_2_western_compressed.png', 
                     p3_prop_western_2_swarm_plot_compressed,
                     width = 100, height = 10, dpi = 300, limitsize = FALSE),
+             format = "file" ),
+  
+  # Generate vertical plot
+  tar_target(
+    p3_prop_western_2_swarm_plot_compressed_vertical,
+    event_swarm_plot_compressed_vertical(swarm_data = p2_prop_western_2_swarm_compressed)
+  ),
+  tar_target(p3_prop_western_2_swarm_compressed_vertical_png,
+             ggsave('src/assets/images/duration-chart/swarm_jd7d_2_western_compressed_vertical.png', 
+                    p3_prop_western_2_swarm_plot_compressed_vertical,
+                    width = 10, height = 100, dpi = 300, limitsize = FALSE),
              format = "file" )
 )
