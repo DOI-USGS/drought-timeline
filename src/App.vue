30228071f2a7236b4650c82aa360dd190062a154
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-11-16 12:59:15 -0600

    turn off pre-footer viz links

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       v-if="!isInternetExplorer"
     /> 
     <PreFooterCodeLinks v-if="!isInternetExplorer" />
-    <PreFooterVisualizationsLinks v-if="!isInternetExplorer" />
+    <!-- PreFooterVisualizationsLinks v-if="!isInternetExplorer" / -->
     <FooterUSGS />
   </div>
 </template>
@@ -25,7 +25,7 @@
             WindowSize,
             HeaderUSGS,
             InternetExplorerPage: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "internet-explorer-page"*/ "./components/InternetExplorerPage"),
-            PreFooterVisualizationsLinks: () => import( /* webpackPreload: true */ /*webpackChunkName: "pre-footer-links-visualizations"*/ "./components/PreFooterVisualizationsLinks"),
+            // PreFooterVisualizationsLinks: () => import( /* webpackPreload: true */ /*webpackChunkName: "pre-footer-links-visualizations"*/ "./components/PreFooterVisualizationsLinks"),
             PreFooterCodeLinks: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "pre-footer-links-code"*/ "./components/PreFooterCodeLinks"),
             FooterUSGS: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "usgs-footer"*/ "./components/FooterUSGS") // Have Webpack put the footer in a separate chunk so we can load it conditionally (with a v-if) if we desire
         },
