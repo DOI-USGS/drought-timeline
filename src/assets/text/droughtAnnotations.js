e074cb2a483030cad4d09e5b317cf3c81cc1d188
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-11-16 17:49:19 -0600

    switch to using js file for text

---
+++ src/assets/text/droughtAnnotations.js
@@ -0,0 +1,22 @@
+export default {
+    timelineEvents: [
+      {
+        id: 'a',
+        date: '1955-01-01',
+        text: 'Event a: 1/1/1955',
+        x_offset_per: 25
+      },
+      {
+        id: 'b',
+        date: '6/1/1980',
+        text: 'Event b: 6/1/1980',
+        x_offset_per: 20
+      },
+      {
+        id: 'c',
+        date: '2012-12-01',
+        text: 'Event c: 12/1/2012',
+        x_offset_per: 65
+      }      
+    ]
+};
