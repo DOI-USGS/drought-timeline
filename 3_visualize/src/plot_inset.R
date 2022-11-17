f68257c9372239dc7e4d11bedb296c13ba1fb7a5
elmeraa <eazadpour@usgs.gov>
2022-11-17 12:14:23 -0800

    adjusted aesthetic bits, removed code for border, and added if statement to add station_data if station = TRUE.

--- 3_visualize/src/plot_inset.R
+++ 3_visualize/src/plot_inset.R
@@ -1,29 +1,23 @@
 
-plot_inset <- function(station_data, western_us_data, us_data){
+plot_inset <- function(station_data, station, western_us_data, us_data, file_png, width, height ){
   
-  ggplot() + 
+  p <- ggplot() + 
     geom_sf(data = us_data,
-            fill = NA,
-            color = 'black') +
+            fill = "white",
+            color = '#7A7A7A') +
     geom_sf(data = western_us_data,
-            fill = '#DDE2E3',
+            fill = '#949494',
             color = NA,
             alpha = 0.5) +
-    #geom_sf(data = station_data) +
     theme_void() 
-    # in the chance you prefer a black border around the inset but still want to remove all unncessary theming elements
-    # theme(axis.text.y   = element_blank(),
-    #       axis.text.x   = element_blank(),
-    #       axis.title.y  = element_blank(),
-    #       axis.title.x  = element_blank(),
-    #       panel.background = element_blank(),
-    #       panel.grid.major = element_blank(), 
-    #       panel.grid.minor = element_blank(),
-    #       axis.line = element_blank(),
-    #       axis.ticks.x = element_blank(),
-    #       axis.ticks.y = element_blank(),
-    #       plot.margin = margin(0, 0, 0, 0, "cm"),
-    #       panel.border = element_rect(color = "black", fill=NA, size=0.75))
+    
+    
+  
+    if (station)
+      p <- p + geom_sf(data = station_data, shape = 21, color = "white", fill = "black", size = 2)
+  
+  ggsave(file_png, width = width, height = height, dpi = 300, bg = "transparent", limitsize = FALSE)
+  return(file_png)
 }
 
 
