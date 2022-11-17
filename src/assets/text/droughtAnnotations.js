28ebc7345c2ef018425f407468ee2d0d3b971fab
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-11-17 16:34:09 -0600

    adjust test annotations

--- src/assets/text/droughtAnnotations.js
+++ src/assets/text/droughtAnnotations.js
@@ -1,22 +1,34 @@
 export default {
     timelineEvents: [
       {
-        id: 'a',
-        date: '1955-01-01',
-        text: 'Event a: 1/1/1955',
+        id: '1',
+        date: '1952-01-01',
+        text: 'Event 1: 1/1/1952',
         x_offset_per: 25
       },
       {
-        id: 'b',
+        id: '4',
         date: '6/1/1980',
-        text: 'Event b: 6/1/1980',
+        text: 'Event 4: 6/1/1980',
         x_offset_per: 20
       },
       {
-        id: 'c',
+        id: '5',
         date: '2012-12-01',
-        text: 'Event c: 12/1/2012',
+        text: 'Event 5: 12/1/2012',
         x_offset_per: 65
-      }      
+      },
+      {
+        id: '3',
+        date: '1955-03-10',
+        text: 'Event 3: 3/10/1955',
+        x_offset_per: 75
+      },
+      {
+        id: '2',
+        date: '1953-02-20',
+        text: 'Event 2: 2/20/1953',
+        x_offset_per: 75
+      }         
     ]
 };
