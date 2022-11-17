668c194a6c808ad77961e9c9340d07165d919577
elmeraa <eazadpour@usgs.gov>
2022-11-16 16:55:41 -0800

    added new R script with function for plotting inset, exported to `3_visualize/out`

--- 3_visualize.R
+++ 3_visualize.R
@@ -1,4 +1,6 @@
 source('3_visualize/src/plot_stripswarm.R')
+source('3_visualize/src/plot_inset.R')
+
 
 p3_targets <- list(
   # Generate plot where each drought day (unique site day with drought)
@@ -23,5 +25,17 @@ p3_targets <- list(
              ggsave('src/assets/images/duration-chart/swarm_jd7d_2_western_compressed.png', 
                     p3_prop_western_2_swarm_plot_compressed,
                     width = 100, height = 10, dpi = 300, limitsize = FALSE),
+             format = "file" ),
+  
+# plotting inset
+  tar_target(p3_inset_map,
+             plot_inset(station_data = p2_prop_western_2_StaID_metadata_sf,
+                        western_us_data = p2_states_western,
+                        us_data = p2_states)),
+  tar_target(p3_inset_map_png,
+             ggsave('3_visualize/out/western_states_inset.png', 
+                    p3_inset_map,
+                    width = 10, height = 10, dpi = 300, bg = "transparent", limitsize = FALSE),
              format = "file" )
+  
 )
