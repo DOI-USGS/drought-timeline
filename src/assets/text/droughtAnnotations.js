3bb36354e59d3a39181263d8d8d33382ed3b6a5c
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-11-18 18:25:00 -0600

    update annotations

--- src/assets/text/droughtAnnotations.js
+++ src/assets/text/droughtAnnotations.js
@@ -2,33 +2,75 @@ export default {
     timelineEvents: [
       {
         id: '1',
-        date: '1952-01-01',
-        text: 'Event 1: 1/1/1952',
-        x_offset_per: 25
+        date: '1954-01-01',
+        text: 'Event 1: 1/1/1954',
+        mobile_x_offset_per: 70,
+        desktop_x_offset_per: 70,
+        desktop_text_anchor: 'start',
+        desktop_text_width: 100,
       },
       {
         id: '4',
-        date: '6/1/1980',
+        date: '1980-06-01',
         text: 'Event 4: 6/1/1980',
-        x_offset_per: 20
+        mobile_x_offset_per: 70,
+        desktop_x_offset_per: 20,
+        desktop_text_anchor: 'start',
+        desktop_text_width: 100
       },
       {
         id: '5',
         date: '2012-12-01',
         text: 'Event 5: 12/1/2012',
-        x_offset_per: 65
+        mobile_x_offset_per: 70,
+        desktop_x_offset_per: 65,
+        desktop_text_anchor: 'start',
+        desktop_text_width: 100
       },
       {
         id: '3',
-        date: '1955-03-10',
-        text: 'Event 3: 3/10/1955',
-        x_offset_per: 75
+        date: '1965-03-10',
+        text: 'Event 3: 3/10/1965',
+        mobile_x_offset_per: 70,
+        desktop_x_offset_per: 75,
+        desktop_text_anchor: 'start',
+        desktop_text_width: 100
       },
       {
         id: '2',
-        date: '1953-02-20',
-        text: 'Event 2: 2/20/1953',
-        x_offset_per: 75
-      }         
+        date: '1955-02-20',
+        text: 'Event 2: 2/20/1955',
+        mobile_x_offset_per: 70,
+        desktop_x_offset_per: 75,
+        desktop_text_anchor: 'start',
+        desktop_text_width: 100
+      },
+      {
+        id: 'intro_text_1',
+        date: '1950-07-01',
+        text: 'Each drought event at a single stream site is represented by a vertical bar. the taller the bar, the longer the drought',
+        mobile_x_offset_per: 20,
+        desktop_x_offset_per: 6,
+        desktop_text_anchor: 'start',
+        desktop_text_width: 340
+      },
+      {
+        id: 'intro_text_3',
+        date: '1952-08-01',
+        text: 'When many sites are experiencing drought at the same time, the chart is really wide',
+        mobile_x_offset_per: 25,
+        desktop_x_offset_per: 6,
+        desktop_text_anchor: 'start',
+        desktop_text_width: 370
+      },
+      {
+        id: 'intro_text_2',
+        date: '1951-07-01',
+        text: 'This drought was 295 days long',
+        mobile_x_offset_per: 40,
+        desktop_x_offset_per: 42,
+        desktop_text_anchor: 'end',
+        desktop_text_width: 370
+      }      
     ]
 };
