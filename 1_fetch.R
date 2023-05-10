9b8788e429e1b706dcab106cbdf47db25444189b
Althea A Archer <aaarcher@usgs.gov>
2023-05-10 09:45:50 -0500

    fetch 1981 and 1921 data

--- 1_fetch.R
+++ 1_fetch.R
@@ -9,7 +9,18 @@ p1_targets <- list(
                                sb_secret_exists = p0_sb_credentials_exist)
   ),
   
-  ##### Fetch data from child item 1951-2020 #####
+  ##### Fetch data from child items #####
+  
+  ###### Download drought properties file ######
+  tar_target(p1_1921_2020_drought_prop_jd_7d_csv, {
+    # Depend on this dummy variable to initiate re-download of files
+    p0_sb_fetch_date
+    download_sb_files(sb_id = p0_sbitem_child_1921_2020,
+                      sb_files_to_download = 'Drought_Properties_jd_07d_wndw.csv',
+                      dest_dir = "1_fetch/out/CONUS_1921_2020",
+                      sb_secret_exists = p0_sb_credentials_exist)
+  },
+  format = 'file'),
   
   ###### Download drought properties file ######
   tar_target(p1_1951_2020_drought_prop_jd_7d_csv, {
@@ -18,6 +29,17 @@ p1_targets <- list(
     download_sb_files(sb_id = p0_sbitem_child_1951_2020,
                       sb_files_to_download = 'Drought_Properties_jd_07d_wndw.csv',
                       dest_dir = "1_fetch/out/CONUS_1951_2020",
+                      sb_secret_exists = p0_sb_credentials_exist)
+  },
+  format = 'file'),
+  
+  ###### Download drought properties file ######
+  tar_target(p1_1981_2020_drought_prop_jd_7d_csv, {
+    # Depend on this dummy variable to initiate re-download of files
+    p0_sb_fetch_date
+    download_sb_files(sb_id = p0_sbitem_child_1981_2020,
+                      sb_files_to_download = 'Drought_Properties_jd_07d_wndw.csv',
+                      dest_dir = "1_fetch/out/CONUS_1981_2020",
                       sb_secret_exists = p0_sb_credentials_exist)
   },
   format = 'file')
