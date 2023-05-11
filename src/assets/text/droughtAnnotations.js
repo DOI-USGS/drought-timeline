4557e159362c00165e13b920bd18923800eb87d7
Althea A Archer <aaarcher@usgs.gov>
2023-05-11 15:18:48 -0500

    update annotations

--- src/assets/text/droughtAnnotations.js
+++ src/assets/text/droughtAnnotations.js
@@ -2,10 +2,10 @@ export default {
     timelineEvents: [
       {
         id: '1', // Unique id for annotation - order and # value don't matter - annotations are later sorted by date
-        date: '1954-01-01', // Date marking start of annotation. Determines y placement on deskop and mobile AND scroll order for annotations on mobile
-        text: 'Event 1: 1/1/1954', // Text of annotation
+        date: '1921-05-01', // Date marking start of annotation. Determines y placement on deskop and mobile AND scroll order for annotations on mobile
+        text: 'Event 1: 10/1/1921', // Text of annotation
         mobile_x_offset_per: 20, // x placement on mobile, as percent of chart width (0 = left edge, 100 = right edge)
-        desktop_x_offset_per: 70, // x placement on desktop, as percent  of chart width
+        desktop_x_offset_per: 40, // x placement on desktop, as percent  of chart width
         desktop_text_anchor: 'start', // alignment of text on desktop (either 'start', 'middle', or 'end')
         desktop_text_width: 100, // Max width allowed for text wrapping
       },
@@ -38,17 +38,17 @@ export default {
       },
       {
         id: '2',
-        date: '1955-02-20',
-        text: 'Event 2: 2/20/1955',
+        date: '1924-03-10',
+        text: 'Longest drought on record, lasted 705 days!',
         mobile_x_offset_per: 20,
-        desktop_x_offset_per: 75,
+        desktop_x_offset_per: 59,
         desktop_text_anchor: 'start',
         desktop_text_width: 100
       },
       {
         id: 'intro_text_1',
-        date: '1950-07-01',
-        text: 'Each drought event at a single stream site is represented by a vertical bar. the taller the bar, the longer the drought',
+        date: '1921-07-01',
+        text: 'Each drought event at a single stream site is represented by a vertical bar. The taller the bar, the longer the drought',
         mobile_x_offset_per: 20,
         desktop_x_offset_per: 6,
         desktop_text_anchor: 'start',
@@ -56,17 +56,17 @@ export default {
       },
       {
         id: 'intro_text_3',
-        date: '1952-07-01',
+        date: '1933-07-01',
         text: 'When many sites are experiencing drought at the same time, the chart is really wide',
         mobile_x_offset_per: 25,
         desktop_x_offset_per: 6,
         desktop_text_anchor: 'start',
-        desktop_text_width: 500
+        desktop_text_width: 400
       },
       {
         id: 'intro_text_2',
-        date: '1951-04-25',
-        text: 'This drought was 281 days long',
+        date: '1923-11-25',
+        text: 'This drought was 126 days long',
         mobile_x_offset_per: 38,
         desktop_x_offset_per: 40,
         desktop_text_anchor: 'end',
