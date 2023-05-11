654d0c8b42a06f330a70f88b3b5a9d51260062c5
Althea A Archer <aaarcher@usgs.gov>
2023-05-11 15:18:02 -0500

    add parameters for website

--- 3_visualize/src/plot_stripswarm.R
+++ 3_visualize/src/plot_stripswarm.R
@@ -75,13 +75,18 @@ event_swarm_plot_compressed_vertical <- function(swarm_data){
   # plot swarm vertically
   p <- swarm_data %>% 
     ggplot()+
-    geom_tile(aes(y=(date+(duration/2)), x=rnum, fill = duration, height=duration), width=0.5)+ # tiles plot centered, so offset x by 1/2 of duration
-    scale_fill_scico(values = scaledBreaks, palette = "lajolla", begin = 0.25, end = 1 , direction = 1,
+    geom_tile(aes(y = (date + (duration/2)), x = rnum, 
+                  fill = duration, height = duration), width = 0.5)+ # tiles plot centered, so offset x by 1/2 of duration
+    scale_fill_scico(values = scaledBreaks, palette = "lajolla", begin = 0.25, end = 1 , 
+                     direction = 1,
                      breaks = c(5, 100, 200, 300))+
     theme_nothing() +
+    #theme_minimal()+ use to check line-up on website
     scale_y_date(breaks = scales::date_breaks(width = '1 years'),
                  labels = NULL,
+                 #labels = scales::date_format('%Y'), use to check line-up on website
+                 limits = c(as.Date("1921-03-15", "%Y-%m-%d"), as.Date("2021-03-31", "%Y-%m-%d")),
                  expand = c(0,0),
-                 position='right')
+                 position = 'right')
   
 }
