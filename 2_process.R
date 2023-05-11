6514088317b20856f0775297d076e888f1bbd9a6
Althea A Archer <aaarcher@usgs.gov>
2023-05-11 15:16:47 -0500

    all CONUS data, 2000 severe

--- 2_process.R
+++ 2_process.R
@@ -5,53 +5,81 @@ p2_targets <- list(
   tar_target(p2_metadata,
              readr::read_csv(p1_metadata_csv, col_types=cols())),
   
-  # Define states that are in the western U.S.
-  tar_target(p2_western_states, 
-             c('AZ','CA','CO','ID','MT','NV','NM','OR','UT','WA','WY')),
+  # Define states that are in each CASC region
+  tar_target(p2_CASCs, 
+             data.frame(
+               STATE = state.abb, NAME = state.name
+             ) |>
+               # remove Alaska, Hawaii
+               filter(! STATE %in% c("AK", "HI")) |>
+               # define CASCs
+               mutate("CASC" = case_when(NAME %in% c("Minnesota", "Iowa", "Missouri", 
+                                                     "Wisconsin", "Illinois", "Indiana", 
+                                                     "Michigan", "Ohio") ~ "Midwest",
+                                         NAME %in% c("Montana", "Wyoming", "Colorado", "North Dakota", 
+                                                     "South Dakota", "Nebraska", "Kansas") ~ "North Central",
+                                         NAME %in% c("Maine", "New Hampshire", "Vermont", "Massachusetts", 
+                                                     "Connecticut", "Rhode Island",
+                                                     "New York", "New Jersey", "Pennsylvania", 
+                                                     "Delaware", "Maryland", "West Virginia", 
+                                                     "Virginia", "Kentucky", "District of Columbia") ~ "Northeast",
+                                         NAME %in% c("Washington", "Oregon", "Idaho") ~ "Northwest",
+                                         NAME %in% c("New Mexico", "Texas", 
+                                                     "Oklahoma", "Louisiana") ~ "South Central",
+                                         NAME %in% c("North Carolina", "South Carolina", "Georgia", "Alabama", 
+                                                     "Mississippi", "Florida", "Tennessee", 
+                                                     "Arkansas") ~ "Southeast",
+                                         NAME %in% c("Arizona", "California", 
+                                                     "Utah", "Nevada") ~ "Southwest",
+                                         TRUE ~ "not sorted")) 
+             ),
   
   ##### Data for 1951-2020 #####
-
-  ###### Get 1951-2020 metadata ######
-  tar_target(p2_1951_2020_metadata,
-             filter(p2_metadata, national_1951) %>%
-               mutate(western_us = STATE %in% p2_western_states)),
   
   ###### Load drought properties ######
   ## Using only variable 7d drought properties
+  tar_target(p2_1921_2020_drought_prop_jd_7d,
+             readr::read_csv(p1_1921_2020_drought_prop_jd_7d_csv, col_types = cols()) %>%
+               mutate(across(c(start, end), ~as.Date(.x, '%Y-%m-%d'))) |>
+               left_join(p2_metadata, by = "StaID")),
   tar_target(p2_1951_2020_drought_prop_jd_7d,
              readr::read_csv(p1_1951_2020_drought_prop_jd_7d_csv, col_types = cols()) %>%
-               mutate(across(c(start, end), ~as.Date(.x, '%Y-%m-%d')))),
+               mutate(across(c(start, end), ~as.Date(.x, '%Y-%m-%d'))) |>
+               left_join(p2_metadata, by = "StaID")),
+  tar_target(p2_1981_2020_drought_prop_jd_7d,
+             readr::read_csv(p1_1981_2020_drought_prop_jd_7d_csv, col_types = cols()) %>%
+               mutate(across(c(start, end), ~as.Date(.x, '%Y-%m-%d'))) |>
+               left_join(p2_metadata, by = "StaID")),
   
   ### Prep drought properties for "strip swarm" duration chart
   # Filter to droughts defined using the 2% threshold
   # and to only drought that occurred in the western U.S.
-  tar_target(p2_prop_western_2,
-             p2_1951_2020_drought_prop_jd_7d %>%
-               filter(threshold == 2) %>%
-               left_join(p2_1951_2020_metadata %>%
-                           select(StaID:STATE, HCDN_2009, western_us)) %>%
-               filter(western_us)),
+  tar_target(p2_all_2pct_droughts,
+             join_full_drought_record(df_1921 = p2_1921_2020_drought_prop_jd_7d,
+                                      df_1951 = p2_1951_2020_drought_prop_jd_7d,
+                                      df_1981 = p2_1981_2020_drought_prop_jd_7d)
+             ),
+  
+  # determine the 2000 most severe droughts
+  tar_target(p2_2000_severe_2pct_droughts,
+             p2_all_2pct_droughts |>
+               slice_max(order_by = severity,
+                         n = 2000) |>
+               mutate(unique_id = paste0(StaID, "-", drought_id))
+             ),
 
   # Identify drought chunks
-  tar_target(p2_prop_western_2_drought_chunks,
-             identify_drought_chunks(p2_prop_western_2, min_chunk_days=365)),
+  tar_target(p2_drought_chunks,
+             identify_drought_chunks(p2_2000_severe_2pct_droughts, 
+                                     min_chunk_days = 365)),
   
   # Process data to generate swarm
-  ## Original approach (# cells filled per event = duration)
-  ## nrow = # of drought days (unique site days with droughts)
-  tar_target(p2_prop_western_2_swarm,
-             create_event_swarm(event_data = p2_prop_western_2,
-                                             start_period = p2_prop_western_2_drought_chunks$start_date,
-                                             end_period = p2_prop_western_2_drought_chunks$break_date,
-                                             max_droughts = p2_prop_western_2_drought_chunks$max_single_day_droughts),
-             pattern = map(p2_prop_western_2_drought_chunks)),
-  
   ## 'Compressed' approach (# cells filled per event = 1)
   ## nrow = # of drought events = nrow(p2_prop_western_2)
-  tar_target(p2_prop_western_2_swarm_compressed,
-             create_event_swarm_compressed(event_data = p2_prop_western_2,
-                                start_period = p2_prop_western_2_drought_chunks$start_date,
-                                end_period = p2_prop_western_2_drought_chunks$break_date,
-                                max_droughts = p2_prop_western_2_drought_chunks$max_single_day_droughts),
-             pattern = map(p2_prop_western_2_drought_chunks))
+  tar_target(p2_drought_swarm_compressed,
+             create_event_swarm_compressed(event_data = p2_2000_severe_2pct_droughts,
+                                start_period = p2_drought_chunks$start_date,
+                                end_period = p2_drought_chunks$break_date,
+                                max_droughts = p2_drought_chunks$max_single_day_droughts),
+             pattern = map(p2_drought_chunks))
 )
