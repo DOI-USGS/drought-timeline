e480fdd66144800cde707863a1701da9f783ed8e
Corson-Dosch <hcorson-dosch@usgs.gov>
2023-05-17 10:28:24 -0500

    Modify plotting snippets to check scale of plot against website scale

--- 3_visualize/src/plot_stripswarm.R
+++ 3_visualize/src/plot_stripswarm.R
@@ -12,11 +12,15 @@ event_swarm_plot_compressed_vertical <- function(swarm_data){
                      direction = 1,
                      breaks = c(5, 100, 200, 300))+
     theme_nothing() +
-    #theme_minimal()+ use to check line-up on website
+    # To check line-up of scales on website, uncomment out lines 17-19, comment
+    # out line 21, and uncomment out line 22
+    # theme(axis.text.x = element_blank(),
+    #       axis.text.y = element_text(size = 14, color = 'blue'),
+    #       panel.grid.major.y = element_line(color = 'blue', linetype = "dashed")) +
     scale_y_date(breaks = scales::date_breaks(width = '1 years'),
                  labels = NULL,
-                 #labels = scales::date_format('%Y'), use to check line-up on website
-                 limits = c(as.Date("1921-03-15", "%Y-%m-%d"), as.Date("2021-03-31", "%Y-%m-%d")),
+                 # labels = scales::date_format('%Y'), # use to check line-up on website
+                 limits = c(as.Date(min(swarm_data$date)), as.Date(max(swarm_data$date))),
                  expand = c(0,0),
                  position = 'right')
   
